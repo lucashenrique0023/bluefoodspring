4bf9ec3a1c20b0bf13ab48fc5c5fe2c7bbd66fa6
detectiverust93 <lucas.henrique0023@gmail.com>
2020-08-11 14:59:16 -0300

    Entidade Cliente, Usuario construida utilizando Lombok, Domain Driven Development e Configuracao das entidades com JPA

---
+++ src/main/java/br/com/softblue/bluefood/domain/cliente/Cliente.java
@@ -0,0 +1,19 @@
+package br.com.softblue.bluefood.domain.cliente;
+
+import javax.persistence.Entity;
+
+import br.com.softblue.bluefood.domain.usuario.Usuario;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+	
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
+public class Cliente extends Usuario {
+
+	private String cpf;
+	
+	private String cep;
+}
---
+++ src/main/java/br/com/softblue/bluefood/domain/usuario/Usuario.java
@@ -0,0 +1,29 @@
+package br.com.softblue.bluefood.domain.usuario;
+
+import java.io.Serializable;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@MappedSuperclass
+public class Usuario implements Serializable {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	private Integer id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String senha;
+	
+	private String telefone;
+}
