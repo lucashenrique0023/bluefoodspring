65694947c472a67963973b8352c4495ff75df568
detectiverust93 <lucas.henrique0023@gmail.com>
2020-08-12 11:35:28 -0300

    * Montando Tela de Cadastro do Cliente 1. Configuracao do Redirecionamento para  ontroler com th:action 2. Conceito de Divisao de Entidades para Autenticacao no Controlador em Public, Cliente, Restaurante 3. Criacao Inicial do PublicController 4. Conteito e Utilizacao do objeto Model do spring
    * Persistindo cliente no banco de dados
    0. Entendendo como persistir com o Spring
    1. Entendendo e Criando classe ClienteRepository
    2. Entendendo e Criando classe ClienteService
    3. Criando metodo para salvar a requisicao post no Controller

---
+++ src/main/java/br/com/softblue/bluefood/domain/cliente/ClienteRepository.java
@@ -0,0 +1,7 @@
+package br.com.softblue.bluefood.domain.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
---
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
@@ -0,0 +1,37 @@
+package br.com.softblue.bluefood.infrastructure.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.softblue.bluefood.application.ClienteService;
+import br.com.softblue.bluefood.domain.cliente.Cliente;
+
+@Controller
+@RequestMapping(path = "/public")
+public class PublicController {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping("/cliente/new")
+	public String newCliente(Model model) {
+		
+		//Cliente cliente = new Cliente();
+		//cliente.setNome("Lucas");
+		model.addAttribute("cliente", new Cliente());
+		return "cliente-cadastro";
+	}
+	
+	@PostMapping(path = "/cliente/save")
+	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
+		clienteService.saveCliente(cliente);
+		
+		return "cliente-cadastro";
+	}
+
+}
