2dc47b3dacf6d8be9fbb4d7362a3943559c75830
detectiverust93 <lucas.henrique0023@gmail.com>
2020-08-13 14:42:13 -0300

    Colocando a validacao para funcionar! Configuramos o Controller para utilizar o @Valid que valida a entidade recebida na requisicao com o bean validation e utilizamos o th:if para exibir a menssagem de sucesso.

--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
@@ -1,8 +1,11 @@
 package br.com.softblue.bluefood.infrastructure.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,14 +27,22 @@ public class PublicController {
 		//Cliente cliente = new Cliente();
 		//cliente.setNome("Lucas");
 		model.addAttribute("cliente", new Cliente());
-		ControllerHelper.setEditMode(model, true);
+		ControllerHelper.setEditMode(model, false);
 		return "cliente-cadastro";
 	}
 	
 	@PostMapping(path = "/cliente/save")
-	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
-		clienteService.saveCliente(cliente);
+	public String saveCliente(@ModelAttribute("cliente") @Valid Cliente cliente,
+			Errors errors,
+			Model model) {
 		
+		if (!errors.hasErrors()) {
+			clienteService.saveCliente(cliente);
+			model.addAttribute("msg", "Cliente gravado com sucesso!");
+		}
+		
+		
+		ControllerHelper.setEditMode(model, false);
 		return "cliente-cadastro";
 	}
 
