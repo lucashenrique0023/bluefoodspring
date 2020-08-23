6b3860b52eded12f4c96ec8cc2a1086828dd0654
detectiverust93 <lucas.henrique0023@gmail.com>
2020-08-23 14:56:06 -0300

    Configurando Filtro Busca - Parte 02
    - Configuramos a taxa de entrega utilizando a forma monetaria brasileira
    atraves da propriedade #numbers.formatCurrency do thymeleaf.
    
    
    - Configuramos o retorno das categorias para a view atraves do metodo
    novo metodo getCategoriaAsText na classe de Restaurante, criamos o
    metodo no dominio do restaurante pois ja tinhamos a lista de categorias
    la, alem disso tambem configuramos o FetchType.EAGER para que o Spring
    Data preencha a lista de categorias visto que e um relacionamento
    ManyToMany e o padrao e o LAZY.
    
    - Configuramos o metodo concatenate no StringUtils para concatenar a
    colecao separando por virgula e retornando um texto.

--- src/main/java/br/com/softblue/bluefood/util/StringUtils.java
+++ src/main/java/br/com/softblue/bluefood/util/StringUtils.java
@@ -1,7 +1,11 @@
 package br.com.softblue.bluefood.util;
 
+import java.util.Collection;
+
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 
 public class StringUtils {
 	
@@ -23,5 +27,26 @@ public class StringUtils {
 	  return enconder.encode(rawString); 
 	  }
 	 
+	  public static String concatenate(Collection<String> strings) {
+		  if (strings == null || strings.size() == 0) {
+			  return null;
+		  }
+		  
+		  StringBuilder sb = new StringBuilder();
+		  String delimiter = ", ";
+		  boolean first = true;
+		  
+		  for (String string : strings) {
+			  
+			  if (!first) {
+				  sb.append(delimiter);
+			  }
+			  
+			  sb.append(string);
+			  first = false;
+		  }
+		  
+		  return sb.toString();	  
+	  }
 
 }
