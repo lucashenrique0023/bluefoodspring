0d5ca7bb63ecc5367ff801d36e3de082609ef413
detectiverust93 <lucas.henrique0023@gmail.com>
2020-08-31 22:52:36 -0300

    Criando o pedido com base nos itens do carrinho
    Preparamos o pedido e persistimos no banco de dados.
    
    1- em cliente-carrinho.html preparamos o formulario que envia o numero
    do cartao de credito e solicita o pagamento.
    
    2- no PagamentoController configuramos o metodo de pagar, que vai
    solicitar ao service que construa o pedido e retorne uma instancia do
    pedido, depois limpamos(destruimos) o carrinho e usamos o redirect para
    que o navegador do cliente solicite a pagina de visualizacao do pedido
    de acordo com seu ID.
    
    3- Criamos em PedidoService, o metodo criarEPagar preenche e gera o
    pedido, em seguida percorre um loop com todos os itens do pedido,
    preenchendo suas chaves primaria com o pedido e a ordem, para em seguida
    salvar o pedido.
    
    4- Criamos o itemPedidoRepository responsavel por armazenar o pedido.
    
    e por fim retornamos o pedido.

---
+++ src/main/java/br/com/softblue/bluefood/domain/pedido/PedidoRepository.java
@@ -0,0 +1,7 @@
+package br.com.softblue.bluefood.domain.pedido;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
