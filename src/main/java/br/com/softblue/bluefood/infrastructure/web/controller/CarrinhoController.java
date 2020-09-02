198ec963e6ba2fe4293528ef0baf3f4bcb907599
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-01 21:57:27 -0300

    - Refazendo um pedido anterior
    Criamos a funcao no sistema onde o usuario pode carregar um novo
    carrinho atraves de um pedido anterior.
    
    1. No controlador CarrinhoController responsavel por receber os dados ja
    preenchidos de uma tela de vizualizacao de um pedido anterior do usuario
    onde vamos pesquisar o pedido anterior pelo ID, criar o pedido e em
    seguida pegar todos os itens daquele pedido e preencher um novo carrinho
    para o cliente.
    
    2. na tela de cliente-pedido inserimos o botao de refazer pedido
    passando como parametro o id do pedido.

--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/CarrinhoController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/CarrinhoController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import br.com.softblue.bluefood.domain.pedido.Carrinho;
+import br.com.softblue.bluefood.domain.pedido.ItemPedido;
+import br.com.softblue.bluefood.domain.pedido.Pedido;
+import br.com.softblue.bluefood.domain.pedido.PedidoRepository;
 import br.com.softblue.bluefood.domain.pedido.RestauranteDiferenteException;
 import br.com.softblue.bluefood.domain.restaurante.ItemCardapio;
 import br.com.softblue.bluefood.domain.restaurante.ItemCardapioRepository;
@@ -23,6 +26,9 @@ public class CarrinhoController {
 
 	@Autowired
 	private ItemCardapioRepository itemCardapioRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	@ModelAttribute("carrinho")
 	public Carrinho carrinho() {
@@ -66,6 +72,23 @@ public class CarrinhoController {
 		
 		if (carrinho.vazio()) {
 			sessionStatus.setComplete(); // Elimina os atributos de sessao (neste caso o carrinho)
+		}
+		
+		return "cliente-carrinho";
+	}
+	
+	@GetMapping(path = "/refazerCarrinho")
+	public String refazerCarrinho(
+			@RequestParam("pedidoId") Integer pedidoId,
+			@ModelAttribute("carrinho") Carrinho carrinho,
+			Model model) {
+		
+		Pedido pedido = pedidoRepository.findById(pedidoId).orElseThrow();
+		
+		carrinho.limpar();
+		
+		for (ItemPedido itemPedido : pedido.getItens()) {
+			carrinho.adicionarItem(itemPedido);
 		}
 		
 		return "cliente-carrinho";
