8c8699404fa63b157fce3fa31c013be8b7786804
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-09 20:42:32 -0300

    Criando o relatório de faturamento de itens de pedidos
    Criamos um relatorio que busca a quantidade de itens vendidas durante um
    intervalo especificado agrupando por quantidade e somando o valor das
    vendas.
    
    1. Criamos o template restaurante-relatorio-itens que exibe a pagina de
    filtro e listagem.
    
    2.Configuramos o RestauranteController padrao
    
    3.  Criamos a classe RelatorioItemFilter para representar o filtro da
    tela.
    
    4. Criamos a classe RelatorioItemFaturamento para representar as colunas
    da consulta
    
    5. No service configuramos a logica de busca , tratamento do resultado
    da consulta e retorno.
    
    6. e no Repository configuramos as consultas com JPQL utilizando Join e
    Group By.

--- src/main/java/br/com/softblue/bluefood/application/service/RelatorioService.java
+++ src/main/java/br/com/softblue/bluefood/application/service/RelatorioService.java
@@ -1,6 +1,9 @@
 package br.com.softblue.bluefood.application.service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import br.com.softblue.bluefood.domain.pedido.Pedido;
 import br.com.softblue.bluefood.domain.pedido.PedidoRepository;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFaturamento;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFilter;
 import br.com.softblue.bluefood.domain.pedido.RelatorioPedidoFilter;
 
 @Service
@@ -37,5 +42,45 @@ public class RelatorioService {
 		}
 		
 		return pedidoRepository.findByDateInterval(restauranteId, dataInicial.atStartOfDay(), dataFinal.atTime(23, 59, 59));
+	}
+	
+	public List<RelatorioItemFaturamento> calcularFaturamentoItens(Integer restauranteId, RelatorioItemFilter filter) {
+		
+		List<Object[]> itensObj;
+		List<RelatorioItemFaturamento> itens;
+		
+		Integer itemId = filter.getItemId();
+		
+		LocalDate dataInicial = filter.getDataInicial();
+		LocalDate dataFinal = filter.getDataFinal();
+		
+		if (dataInicial == null) {
+			return List.of();
+		}
+		
+		if (dataFinal == null) {
+			dataFinal = LocalDate.now();
+		}
+		
+		LocalDateTime dataHoraInicial = dataInicial.atStartOfDay();
+		LocalDateTime dataHoraFinal = dataFinal.atTime(23, 59, 59);
+		
+		if (itemId != 0) {
+			itensObj = pedidoRepository.findItensPorFaturamento(restauranteId, itemId, dataHoraInicial, dataHoraFinal);
+		} else {
+			itensObj = pedidoRepository.findItensPorFaturamento(restauranteId, dataHoraInicial, dataHoraFinal);
+		}
+		
+		itens = new ArrayList<>();
+		
+		for (Object[] item : itensObj) {
+			String nome = (String) item[0];
+			Long quantidade = (Long) item[1];
+			BigDecimal valor = (BigDecimal) item[2];
+			itens.add(new RelatorioItemFaturamento(nome, quantidade, valor));
+		}
+		
+		return itens;
+		
 	}
 }
--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/RestauranteController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/RestauranteController.java
@@ -19,6 +19,8 @@ import br.com.softblue.bluefood.application.service.RestauranteService;
 import br.com.softblue.bluefood.application.service.ValidationException;
 import br.com.softblue.bluefood.domain.pedido.Pedido;
 import br.com.softblue.bluefood.domain.pedido.PedidoRepository;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFaturamento;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFilter;
 import br.com.softblue.bluefood.domain.pedido.RelatorioPedidoFilter;
 import br.com.softblue.bluefood.domain.restaurante.CategoriaRestauranteRepository;
 import br.com.softblue.bluefood.domain.restaurante.ItemCardapio;
@@ -164,6 +166,23 @@ public class RestauranteController {
 		model.addAttribute("filter", filter);
 		
 		return "restaurante-relatorio-pedidos";
+	}
+	
+	@GetMapping(path = "/relatorio/itens")
+	public String relatorioItens(
+			@ModelAttribute("relatorioItemFilter") RelatorioItemFilter filter,
+			Model model) {
+		Integer restauranteId = SecurityUtils.loggedRestaurante().getId();
+		List<ItemCardapio> itensCardapio = itemCardapioRepository.findByRestaurante_IdOrderByNome(restauranteId);
+
+		model.addAttribute("itensCardapio", itensCardapio);
+		
+		List<RelatorioItemFaturamento> itensCalculados = relatorioService.calcularFaturamentoItens(restauranteId, filter);
+		model.addAttribute("itensCalculados", itensCalculados);
+		
+		model.addAttribute("relatorioItemFilter", filter);
+		
+		return "restaurante-relatorio-itens";
 	}
 
 }
