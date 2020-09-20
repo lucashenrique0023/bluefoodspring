f3b1bc013189a9f2b384dcb82c0b636d7a98e25a
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-19 22:18:29 -0300

    Testes Unitarios - Parte 01
    Configuracao do Ambiente para receber os testes.
    
    Os testes devem ser executados em um ambiente limpo, livre de alteracoes
    de dados.
    
    1. Configuramos o banco de dados h2 no gradle, que testa diretamente na
    memoria ram e apaga quando finaliza os testes.
    
    2. Criamos um profile de teste e configuramos o h2, alem de ajustar os
    outros profiles.
    
    3. Ajustamos a classe que insere dados para teste apenas quando for o
    perfil de desenvolvimento.
    
    4. Criamos em Properties -> JavaBuildPath -> Source, dois diretorios
    para teste
    
    src/test/resources e src/test/java
    
    e por fim criamos o pacote do br.com.softblue.bluefood em test/java e
    criamos uma classe de testes utilizando o Juniper, apenas para testar a
    execucao.

--- src/main/java/br/com/softblue/bluefood/application/test/InsertDataForTesting.java
+++ src/main/java/br/com/softblue/bluefood/application/test/InsertDataForTesting.java
@@ -9,6 +9,8 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.stereotype.Component;
 
 import br.com.softblue.bluefood.domain.cliente.Cliente;
@@ -44,20 +46,28 @@ public class InsertDataForTesting {
 
 	@EventListener
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		Cliente[] clientes = clientes();
-		Restaurante[] restaurantes = restaurantes();
-		itensCardapio(restaurantes);
 		
-		Pedido p = new Pedido();
-		p.setData(LocalDateTime.now());
-		p.setCliente(clientes[0]);
-		p.setRestaurante(restaurantes[0]);
-		p.setStatus(Status.Producao);
-		p.setSubtotal(BigDecimal.valueOf(10));
-		p.setTaxaEntrega(BigDecimal.valueOf(2));
-		p.setTotal(BigDecimal.valueOf(12.0));
-		pedidoRepository.save(p);
+		// Identifica Profile e so executa se for Dev
 		
+		Environment environment = event.getApplicationContext().getEnvironment();
+		
+		if (environment.acceptsProfiles(Profiles.of("dev"))) {
+			
+			Cliente[] clientes = clientes();
+			Restaurante[] restaurantes = restaurantes();
+			itensCardapio(restaurantes);
+			
+			Pedido p = new Pedido();
+			p.setData(LocalDateTime.now());
+			p.setCliente(clientes[0]);
+			p.setRestaurante(restaurantes[0]);
+			p.setStatus(Status.Producao);
+			p.setSubtotal(BigDecimal.valueOf(10));
+			p.setTaxaEntrega(BigDecimal.valueOf(2));
+			p.setTotal(BigDecimal.valueOf(12.0));
+			pedidoRepository.save(p);
+			
+		}
 	}
 	
 	private Restaurante[] restaurantes() {
---
+++ src/test/java/br/com/softblue/bluefood/CheckingTest.java
@@ -0,0 +1,13 @@
+package br.com.softblue.bluefood;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckingTest {
+
+	@Test
+	public void testSomething() throws Exception {
+		// Teste qualquer
+		
+	}
+
+}
