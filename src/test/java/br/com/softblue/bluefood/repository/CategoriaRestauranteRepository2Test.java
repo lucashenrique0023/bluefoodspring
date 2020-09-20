38abfa26fe0006dbc1421bda6010f5a3c02db4e8
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-20 11:37:58 -0300

    Outra forma de testar repository - Parte 03
    Aqui testamos a classe repository com o objeto TestEntityManager e
    tambem criamos uma configuracao em Run Configurations onde rodamos todos
    os testes ao mesmo tempo.

---
+++ src/test/java/br/com/softblue/bluefood/repository/CategoriaRestauranteRepository2Test.java
@@ -0,0 +1,47 @@
+package br.com.softblue.bluefood.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.softblue.bluefood.domain.restaurante.CategoriaRestaurante;
+import br.com.softblue.bluefood.domain.restaurante.CategoriaRestauranteRepository;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class CategoriaRestauranteRepository2Test {
+
+	@Autowired
+	private TestEntityManager em;
+	
+	@Test
+	public void testInsertAndDelete() throws Exception {
+		
+		assertThat(em).isNotNull();
+		
+		CategoriaRestaurante cr = new CategoriaRestaurante();
+		cr.setNome("Chines");
+		cr.setImagem("chinesa.png");
+		em.persistAndFlush(cr);
+		
+		assertThat(cr.getId()).isNotNull();
+		
+		CategoriaRestaurante cr2 =  em.find(CategoriaRestaurante.class, cr.getId());
+		
+		assertThat(cr.getNome()).isEqualTo(cr2.getNome());
+		
+		//assertThat(categoriaRestauranteRepository.findAll()).hasSize(7); // data.sql insere 6.
+		
+		em.remove(cr);
+		
+		assertThat(em.find(CategoriaRestaurante.class, cr.getId())).isNull();
+		
+		
+	}
+}
