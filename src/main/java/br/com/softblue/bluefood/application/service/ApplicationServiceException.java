baa8ef64789967ebef92a482c10c3b66749e8509
detectiverust93 <lucas.henrique0023@gmail.com>
2020-10-11 13:11:49 -0300

    Configurando Sistema de migracao Flyway
    - Controlar versoes em nosso banco de dados.

--- src/main/java/br/com/softblue/bluefood/application/service/ApplicationServiceException.java
+++ src/main/java/br/com/softblue/bluefood/application/service/ApplicationServiceException.java
@@ -1,12 +1,12 @@
 package br.com.softblue.bluefood.application.service;
 
-public class ApplicationServiceException extends RuntimeException {
 
+public class ApplicationServiceException extends RuntimeException {
 	public ApplicationServiceException(String message, Throwable cause) {
 		super(message, cause);
 		
 	}
-
+	
 	public ApplicationServiceException(String message) {
 		super(message);
 		
